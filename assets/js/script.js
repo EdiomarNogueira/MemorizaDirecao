a1bf35f6a4fce708250f422964ad1dbbb973ef8a
Ediomar Nogueira <ediomar_ti@hotmail.com>
2021-10-26 00:18:13 -0300

    Versão 0.2 Inclusão de tratamento das marchas

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,143 +14,12 @@ let carros2 = [
 ]
 */
 
-function trocaimagem(filename, nomeImagem) {
-    document.querySelector('.imagem-acao--gif').setAttribute('src', '/assets/images/' + filename);
-    document.querySelector('.imagem-acao--gif').setAttribute('alt', nomeImagem);
+function motorMorre() {
+    document.getElementById("statusCarro").innerHTML = carro.nome + " Motor Morreu";
+    document.getElementById("acaoCarro").innerHTML = "O Carro Precisa de Mais Aceleração Para Subir a Marcha";
+    trocaimagem('morreu.gif', 'Imagem Carro Pegando Fogo');
+};
 
-}
-
-function trocamensagem(titulo, mensagem) {
-    document.getElementById("statusCarro").innerHTML = carro.nome + titulo;
-    document.getElementById("acaoCarro").innerHTML = mensagem;
-}
-
-function trocastatus(campo, mensagem) {
-    document.getElementById(campo).innerHTML = mensagem;
-}
-
-marchaatual = 0;
-document.querySelector('#neutro').classList.add('acionado');
-
-function mudamarcha(novamarcha) {
-    if (novamarcha == (marchaatual + 1)) {
-        switch (novamarcha) {
-            case -1:
-                document.querySelector('#re').classList.add('acionado');
-                document.getElementById("acaoCarro").innerHTML = "";
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ré";
-                document.getElementById("acaoCarro").innerHTML = "E la vai de moonwalk";
-
-                marchaatual = novamarcha;
-                break;
-            case 0:
-                document.querySelector('#neutro').classList.add('acionado');
-                document.querySelector('#re').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ponto Morto";
-                document.getElementById("acaoCarro").innerHTML = "E ai, qual vai ser?";
-                marchaatual = novamarcha;
-
-                break;
-            case 1:
-                document.querySelector('#primeira').classList.add('acionado');
-                document.querySelector('#neutro').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Primeira";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 15km/h";
-
-                marchaatual = novamarcha;
-
-                break;
-            case 2:
-                document.querySelector('#segunda').classList.add('acionado');
-                document.querySelector('#primeira').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Segunda";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 25km/h";
-
-                marchaatual = novamarcha;
-
-                break;
-            case 3:
-                document.querySelector('#terceira').classList.add('acionado');
-                document.querySelector('#segunda').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Terceira";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 45km/h";
-
-                marchaatual = novamarcha;
-
-                break;
-            case 4:
-                document.querySelector('#quarta').classList.add('acionado');
-                document.querySelector('#terceira').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quarta";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 60km/h";
-
-                marchaatual = novamarcha;
-
-                break;
-            case 5:
-                document.querySelector('#quinta').classList.add('acionado');
-                document.querySelector('#quarta').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quinta";
-                document.getElementById("acaoCarro").innerHTML = "Ta Com Pressa Querido(a)?";
-                marchaatual = novamarcha;
-                break;
-        }
-    } else if (novamarcha == (marchaatual - 1)) {
-        switch (novamarcha) {
-            case -1:
-                document.querySelector('#re').classList.add('acionado');
-                document.querySelector('#neutro').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ré";
-                document.getElementById("acaoCarro").innerHTML = "E la vai de moonwalk";
-                marchaatual = novamarcha;
-                break;
-            case 0:
-                document.querySelector('#neutro').classList.add('acionado');
-                document.querySelector('#primeira').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ponto Morto";
-                document.getElementById("acaoCarro").innerHTML = "E ai, qual vai ser?";
-                marchaatual = novamarcha;
-                break;
-            case 1:
-                document.querySelector('#primeira').classList.add('acionado');
-                document.querySelector('#segunda').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Primeira";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 15km/h";
-                marchaatual = novamarcha;
-                break;
-            case 2:
-                document.querySelector('#segunda').classList.add('acionado');
-                document.querySelector('#terceira').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Segunda";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 25km/h";
-                marchaatual = novamarcha;
-                break;
-            case 3:
-                document.querySelector('#terceira').classList.add('acionado');
-                document.querySelector('#quarta').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Terceira";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 45km/h";
-                marchaatual = novamarcha;
-                break;
-            case 4:
-                document.querySelector('#quarta').classList.add('acionado');
-                document.querySelector('#quinta').classList.remove('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quarta";
-                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 60km/h";
-                marchaatual = novamarcha;
-                break;
-            case 5:
-                document.querySelector('#quinta').classList.add('acionado');
-                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quinta";
-                document.getElementById("acaoCarro").innerHTML = "Ta Com Pressa Querido(a)?";
-                marchaatual = novamarcha;
-                break;
-        }
-    } else {
-        document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha Fora da Sequência";
-        document.getElementById("acaoCarro").innerHTML = "É recomendado trocar a marcha de acordo com a sequência";
-    }
-}
 
 
 let valorrotacao = 0;
@@ -173,7 +42,12 @@ function valoraceleracao(rotacao) {
         trocastatus('acelerador', 'Acelerando');
         document.querySelector('#acelerador').classList.add('acionado')
 
+    } else if (valorrotacao == 0) {
+        trocastatus('acelerador', 'Livre');
+        document.querySelector('#acelerador').classList.remove('acionado')
     }
+
+
     console.log(valorrotacao);
     document.querySelector('#rotacoes').value = valorrotacao;
     document.querySelector('.progress-bar-aceleracao').style.setProperty('--progress', valorrotacao);
@@ -224,6 +98,223 @@ function pressaoembreagem(pressao) {
     document.querySelector('.progress-bar-embreagem').style.setProperty('--progress', valorpressao);
 
 };
+
+function trocaimagem(filename, nomeImagem) {
+    document.querySelector('.imagem-acao--gif').setAttribute('src', '/assets/images/' + filename);
+    document.querySelector('.imagem-acao--gif').setAttribute('alt', nomeImagem);
+
+}
+
+function trocamensagem(titulo, mensagem) {
+    document.getElementById("statusCarro").innerHTML = carro.nome + titulo;
+    document.getElementById("acaoCarro").innerHTML = mensagem;
+}
+
+function trocastatus(campo, mensagem) {
+    document.getElementById(campo).innerHTML = mensagem;
+}
+
+marchaatual = 0;
+document.querySelector('#neutro').classList.add('acionado');
+
+function mudamarcha(novamarcha) {
+    if (novamarcha == (marchaatual + 1)) {
+        switch (novamarcha) {
+            case -1:
+
+                this.re = true;
+                document.querySelector('#re').classList.add('acionado');
+                document.getElementById("acaoCarro").innerHTML = "";
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ré";
+                document.getElementById("acaoCarro").innerHTML = "E la vai de moonwalk";
+                trocaimagem('re.gif', 'Carro dando ré');
+                marchaatual = novamarcha;
+                break;
+            case 0:
+                this.re = false;
+                this.neutro = true;
+                document.querySelector('#neutro').classList.add('acionado');
+                document.querySelector('#re').classList.remove('acionado');
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ponto Morto";
+                document.getElementById("acaoCarro").innerHTML = "E ai, qual vai ser?";
+                marchaatual = novamarcha;
+                break;
+            case 1:
+                if (valorrotacao < 20) {
+                    console.log("aceleracao insuficiente");
+                    motorMorre();
+                } else {
+                    valoraceleracao(-20);
+                    this.neutro = false;
+                    this.primeira = true;
+                    document.querySelector('#primeira').classList.add('acionado');
+                    document.querySelector('#neutro').classList.remove('acionado');
+                    document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Primeira";
+                    document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 15km/h";
+                    trocaimagem('primeira.gif', 'Na primeira marcha');
+                    marchaatual = novamarcha;
+                }
+                break;
+            case 2:
+                if (valorrotacao < 40) {
+                    console.log("aceleracao insuficiente");
+                    motorMorre();
+                } else {
+                    valoraceleracao(-20);
+                    this.primeira = false;
+                    this.segunda = true;
+                    document.querySelector('#segunda').classList.add('acionado');
+                    document.querySelector('#primeira').classList.remove('acionado');
+                    document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Segunda";
+                    document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 25km/h";
+                    trocaimagem('segunda.gif', 'Na segunda marcha');
+                    marchaatual = novamarcha;
+                }
+                break;
+            case 3:
+                if (valorrotacao < 40) {
+                    console.log("aceleracao insuficiente");
+                    motorMorre();
+                } else {
+                    valoraceleracao(-20);
+                    this.segunda = false;
+                    this.terceira = true;
+                    document.querySelector('#terceira').classList.add('acionado');
+                    document.querySelector('#segunda').classList.remove('acionado');
+                    document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Terceira";
+                    document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 45km/h";
+                    trocaimagem('terceira.gif', 'Na terceira marcha');
+                    marchaatual = novamarcha;
+                }
+                break;
+            case 4:
+                if (valorrotacao < 40) {
+                    console.log("aceleracao insuficiente");
+                    motorMorre();
+                } else {
+                    valoraceleracao(-20);
+                    this.terceira = false;
+                    this.quarta = true;
+                    document.querySelector('#quarta').classList.add('acionado');
+                    document.querySelector('#terceira').classList.remove('acionado');
+                    document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quarta";
+                    document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 60km/h";
+                    trocaimagem('quarta.gif', 'Na quarta marcha');
+                    marchaatual = novamarcha;
+                }
+                break;
+            case 5:
+                if (valorrotacao < 40) {
+                    console.log("aceleracao insuficiente");
+                    motorMorre();
+                } else {
+                    valoraceleracao(-20);
+                    this.quarta = false;
+                    this.quinta = true;
+                    document.querySelector('#quinta').classList.add('acionado');
+                    document.querySelector('#quarta').classList.remove('acionado');
+                    document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quinta";
+                    document.getElementById("acaoCarro").innerHTML = "Ta Com Pressa Querido(a)?";
+                    trocaimagem('quinta.gif', 'Na quinta marcha');
+                    marchaatual = novamarcha;
+                }
+                break;
+        }
+    } else if (novamarcha == (marchaatual - 1)) {
+        switch (novamarcha) {
+            case -1:
+                this.re = true;
+                this.neutro = false;
+                document.querySelector('#re').classList.add('acionado');
+                document.querySelector('#neutro').classList.remove('acionado');
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ré";
+                document.getElementById("acaoCarro").innerHTML = "E la vai de moonwalk";
+                trocaimagem('re.gif', 'Carro dando ré');
+                marchaatual = novamarcha;
+                break;
+
+            case 0:
+                if (valorrotacao > 0) {
+                    document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Neutro";
+                    document.getElementById("acaoCarro").innerHTML = "DESACELERAAAAAAAAAAAAAAA";
+                    trocaimagem('neutroacelerado.gif', 'Imagem de Susto');
+                } else {
+                    valoraceleracao(20);
+                    this.neutro = true;
+                    this.primeira = false;
+                    document.querySelector('#neutro').classList.add('acionado');
+                    document.querySelector('#primeira').classList.remove('acionado');
+                    document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Ponto Morto";
+                    document.getElementById("acaoCarro").innerHTML = "E ai, qual vai ser?";
+                    marchaatual = novamarcha;
+                }
+                break;
+            case 1:
+                valoraceleracao(20);
+                this.segunda = false;
+                this.primeira = true;
+                document.querySelector('#primeira').classList.add('acionado');
+                document.querySelector('#segunda').classList.remove('acionado');
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Primeira";
+                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 15km/h";
+                trocaimagem('primeira.gif', 'Na primeira marcha');
+                marchaatual = novamarcha;
+                break;
+            case 2:
+                valoraceleracao(20);
+                this.segunda = true;
+                this.terceira = false;
+                document.querySelector('#segunda').classList.add('acionado');
+                document.querySelector('#terceira').classList.remove('acionado');
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Segunda";
+                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 25km/h";
+                trocaimagem('segunda.gif', 'Na segunda marcha');
+                marchaatual = novamarcha;
+                break;
+            case 3:
+                valoraceleracao(20);
+                this.quarta = false;
+                this.terceira = true;
+                document.querySelector('#terceira').classList.add('acionado');
+                document.querySelector('#quarta').classList.remove('acionado');
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Terceira";
+                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 45km/h";
+                trocaimagem('terceira.gif', 'Na terceira marcha');
+                marchaatual = novamarcha;
+                break;
+            case 4:
+                valoraceleracao(20);
+                this.quarta = true;
+                this.quinta = false;
+                document.querySelector('#quarta').classList.add('acionado');
+                document.querySelector('#quinta').classList.remove('acionado');
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quarta";
+                document.getElementById("acaoCarro").innerHTML = "Aqui anda até uns 60km/h";
+                trocaimagem('quarta.gif', 'Na quarta marcha');
+                marchaatual = novamarcha;
+                break;
+            case 5:
+                this.quinta = true;
+                document.querySelector('#quinta').classList.add('acionado');
+                document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha - Quinta";
+                document.getElementById("acaoCarro").innerHTML = "Ta Com Pressa Querido(a)?";
+                trocaimagem('quinta.gif', 'Na quinta marcha');
+                marchaatual = novamarcha;
+                break;
+        }
+
+    } else {
+        document.getElementById("statusCarro").innerHTML = carro.nome + " Troca de Marcha Fora da Sequência";
+        document.getElementById("acaoCarro").innerHTML = "É recomendado trocar a marcha de acordo com a sequência";
+
+    }
+
+
+
+
+}
+
+
 
 
 // 
